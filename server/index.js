2a104822b82bfe621f0d1823ad8a88c6176e879e
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-10-14 22:48:02 +0530

    Added functionaltiy to end the game

--- server/index.js
+++ server/index.js
@@ -97,6 +97,24 @@ io.on('connection', (socket) => {
                 console.log(e);
             }
         })
+
+        socket.on('winner', async ({winnerSocketId, roomId}) => {
+            try {
+                let room = await Room.findById(roomId)
+                let player = room.players.find((player) => player.socketID == winnerSocketId);
+                player.points+=1;
+                room = await room.save();
+
+                if(player.points >= room.maxRounds) {
+                    io.to(roomId).emit('endGame', player);
+                }
+                else {
+                    io.to(roomId).emit('pointIncrease', player);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        })
     })
 });
  
